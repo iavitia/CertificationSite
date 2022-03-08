77a7b2ecf7bba8348d984a7ab130808bc04e13de
Isacc <iavitia@outlook.com>
2022-03-07 19:32:56 -0800

    login & register formik validation

--- frontend/src/components/LoginForm.js
+++ frontend/src/components/LoginForm.js
@@ -1,29 +1,88 @@
-import { Box, Stack, TextField } from '@mui/material';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+// form
+import * as Yup from 'yup';
+import { useFormik, Form, FormikProvider } from 'formik';
+// components
+import { Stack, TextField, IconButton, InputAdornment } from '@mui/material';
 import { LoadingButton } from '@mui/lab';
+import Iconify from './Iconify';
 
 const LoginForm = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const navigate = useNavigate();
+
+  const LoginSchema = Yup.object().shape({
+    email: Yup.string()
+      .email('Enter a valid email address')
+      .required('Email is required'),
+    password: Yup.string().required('Password is required'),
+  });
+
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+    validationSchema: LoginSchema,
+    onSubmit: () => {
+      navigate('/dashboard', { replace: true });
+    },
+  });
+
+  const handleShowPassword = () => {
+    setShowPassword((show) => !show);
+  };
+
+  const { errors, touched, isSubmitting, handleSubmit, getFieldProps } = formik;
+
   return (
-    <Box component="form" autoComplete="off" noValidate>
-      <Stack spacing={3}>
-        <TextField
-          fullWidth
-          autoComplete="username"
-          type="email"
-          label="Email address"
-        />
+    <FormikProvider value={formik}>
+      <Form autoComplete="off" noValidate onSubmit={handleSubmit}>
+        <Stack spacing={3}>
+          <TextField
+            fullWidth
+            autoComplete="username"
+            type="email"
+            label="Email address"
+            {...getFieldProps('email')}
+            error={Boolean(touched.email && errors.email)}
+            helperText={touched.email && errors.email}
+          />
 
-        <TextField
-          fullWidth
-          autoComplete="current-password"
-          type={'password'}
-          label="Password"
-        />
+          <TextField
+            fullWidth
+            autoComplete="current-password"
+            type={showPassword ? 'text' : 'password'}
+            label="Password"
+            {...getFieldProps('password')}
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton onClick={handleShowPassword} edge="end">
+                    <Iconify
+                      icon={showPassword ? 'eva:eye-fill' : 'eva:eye-off-fill'}
+                    />
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
+            error={Boolean(touched.password && errors.password)}
+            helperText={touched.password && errors.password}
+          />
 
-        <LoadingButton fullWidth size="large" type="submit" variant="contained">
-          Login
-        </LoadingButton>
-      </Stack>
-    </Box>
+          <LoadingButton
+            fullWidth
+            size="large"
+            type="submit"
+            variant="contained"
+            loading={isSubmitting}
+          >
+            Login
+          </LoadingButton>
+        </Stack>
+      </Form>
+    </FormikProvider>
   );
 };
 
--- frontend/src/components/RegisterForm.js
+++ frontend/src/components/RegisterForm.js
@@ -1,31 +1,102 @@
-import { Box, Stack, TextField } from '@mui/material';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+// form
+import * as Yup from 'yup';
+import { useFormik, Form, FormikProvider } from 'formik';
+// components
+import { Stack, TextField, InputAdornment, IconButton } from '@mui/material';
 import { LoadingButton } from '@mui/lab';
+import Iconify from './Iconify';
 
 const Register = () => {
+  const navigate = useNavigate();
+  const [showPassword, setShowPassword] = useState(false);
+
+  const RegisterSchema = Yup.object().shape({
+    username: Yup.string()
+      .required('Username is required')
+      .min(3, 'Username must be at least 3 characters')
+      .max(30, 'Username must be no more than 30 characters'),
+    email: Yup.string()
+      .required('Email is required')
+      .email('Enter a valid email address'),
+    password: Yup.string()
+      .required('Password is required')
+      .min(8, 'Password must be at least 8 characters'),
+  });
+
+  const formik = useFormik({
+    initialValues: {
+      username: '',
+      email: '',
+      password: '',
+    },
+    validationSchema: RegisterSchema,
+    onSubmit: () => {
+      navigate('/dashboard', { replace: true });
+    },
+  });
+
+  const { errors, touched, handleSubmit, isSubmitting, getFieldProps } = formik;
+
   return (
-    <Box component="form" autoComplete="off" noValidate>
-      <Stack spacing={3}>
-        <TextField fullWidth label="Username" autoComplete="username" />
+    <FormikProvider value={formik}>
+      <Form autoComplete="off" noValidate onSubmit={handleSubmit}>
+        <Stack spacing={3}>
+          <TextField
+            fullWidth
+            label="Username"
+            {...getFieldProps('username')}
+            error={Boolean(touched.username && errors.username)}
+            helperText={touched.username && errors.username}
+          />
 
-        <TextField
-          fullWidth
-          autoComplete="email"
-          type="email"
-          label="Email address"
-        />
+          <TextField
+            fullWidth
+            autoComplete="username"
+            type="email"
+            label="Email address"
+            {...getFieldProps('email')}
+            error={Boolean(touched.email && errors.email)}
+            helperText={touched.email && errors.email}
+          />
 
-        <TextField
-          fullWidth
-          autoComplete="current-password"
-          type={'password'}
-          label="Password"
-        />
+          <TextField
+            fullWidth
+            autoComplete="current-password"
+            type={showPassword ? 'text' : 'password'}
+            label="Password"
+            {...getFieldProps('password')}
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton
+                    edge="end"
+                    onClick={() => setShowPassword((prev) => !prev)}
+                  >
+                    <Iconify
+                      icon={showPassword ? 'eva:eye-fill' : 'eva:eye-off-fill'}
+                    />
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
+            error={Boolean(touched.password && errors.password)}
+            helperText={touched.password && errors.password}
+          />
 
-        <LoadingButton fullWidth size="large" type="submit" variant="contained">
-          Register
-        </LoadingButton>
-      </Stack>
-    </Box>
+          <LoadingButton
+            fullWidth
+            size="large"
+            type="submit"
+            variant="contained"
+            loading={isSubmitting}
+          >
+            Register
+          </LoadingButton>
+        </Stack>
+      </Form>
+    </FormikProvider>
   );
 };
 
