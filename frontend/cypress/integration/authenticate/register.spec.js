b9c11c241f57e138a9e79f79f3967c45fb9b11a0
Isacc <iavitia@outlook.com>
2022-03-09 23:32:11 -0800

    Cypress setup, login & register test

---
+++ frontend/cypress/integration/authenticate/register.spec.js
@@ -0,0 +1,85 @@
+/// <reference types="cypress"/>
+
+describe("Login", () => {
+  beforeEach(() => {
+    cy.visit("/register")
+  })
+
+  it("Register successful", () => {
+    cy.fixture("userData").then((data) => {
+      cy.sel("username").type(data.username)
+      cy.sel("email").type(data.email)
+      cy.sel("password").type(data.password)
+      cy.sel("submitRegister").click()
+      cy.url().should("include", "/dashboard")
+    })
+  })
+
+  it("Empty input errors", () => {
+    cy.url().should("contain", "/register")
+
+    cy.sel("submitRegister").click()
+
+    cy.url().should("contain", "/register")
+    cy.muiError(1, "Username is required")
+    cy.muiError(2, "Email is required")
+    cy.muiError(3, "Password is required")
+  })
+
+  it("Username - invalid special characters", () => {
+    cy.fixture("userData").then((data) => {
+      cy.sel("username").type(data.usernameBadChar)
+      cy.sel("email").type(data.email)
+      cy.sel("password").type(data.password)
+      cy.sel("submitRegister").click()
+
+      cy.muiError(
+        1,
+        "Letters, numbers, dashes, and underscores only. Please try again without symbols."
+      )
+    })
+  })
+
+  it("Username - too short", () => {
+    cy.fixture("userData").then((data) => {
+      cy.sel("username").type(data.usernameShort)
+      cy.sel("email").click().type(data.email)
+      cy.sel("password").type(data.password)
+      cy.sel("submitRegister").click()
+
+      cy.muiError(1, "Username must be at least 3 characters")
+    })
+  })
+  it("Username - too long", () => {
+    cy.fixture("userData").then((data) => {
+      cy.sel("username").type(data.usernameLong)
+      cy.sel("email").click().type(data.email)
+      cy.sel("password").type(data.password)
+      cy.sel("submitRegister").click()
+
+      cy.muiError(1, "Username cannot be longer than 20 characters")
+    })
+  })
+
+  it("Email - invalid email", () => {
+    cy.fixture("userData").then((data) => {
+      cy.sel("username").type(data.username)
+      cy.sel("email").click().type(data.emailBad)
+      cy.sel("password").type(data.password)
+      cy.sel("submitRegister").click()
+
+      cy.muiError(2, "Enter a valid email address")
+    })
+  })
+
+  it("Password - Too short", () => {
+    cy.fixture("userData").then((data) => {
+      cy.sel("username").type(data.username)
+      cy.sel("email").click().type(data.email)
+      cy.sel("password").type(data.passwordShort)
+      cy.sel("submitRegister").click()
+
+      cy.muiError(3, "Password must be at least 8 characters")
+    })
+  })
+})
--- frontend/src/components/RegisterForm.js
+++ frontend/src/components/RegisterForm.js
@@ -1,72 +1,79 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
 // form
-import * as Yup from 'yup';
-import { useFormik, Form, FormikProvider } from 'formik';
+import * as Yup from "yup"
+import { useFormik, Form, FormikProvider } from "formik"
 // components
-import { Stack, TextField, InputAdornment, IconButton } from '@mui/material';
-import { LoadingButton } from '@mui/lab';
-import Iconify from './Iconify';
+import { Stack, TextField, InputAdornment, IconButton } from "@mui/material"
+import { LoadingButton } from "@mui/lab"
+import Iconify from "./Iconify"
 
 const Register = () => {
-  const navigate = useNavigate();
-  const [showPassword, setShowPassword] = useState(false);
+  const navigate = useNavigate()
+  const [showPassword, setShowPassword] = useState(false)
 
   const RegisterSchema = Yup.object().shape({
     username: Yup.string()
-      .required('Username is required')
-      .min(3, 'Username must be at least 3 characters')
-      .max(30, 'Username must be no more than 30 characters'),
+      .min(3, "Username must be at least 3 characters")
+      .max(20, "Username cannot be longer than 20 characters")
+      .required("Username is required")
+      .matches(
+        /^[A-Za-z0-9_-]*$/,
+        "Letters, numbers, dashes, and underscores only. Please try again without symbols."
+      ),
+    // .matches(/^.{3,20}$/s, "Username must be between 3 and 20 characters"),
     email: Yup.string()
-      .required('Email is required')
-      .email('Enter a valid email address'),
+      .required("Email is required")
+      .email("Enter a valid email address"),
     password: Yup.string()
-      .required('Password is required')
-      .min(8, 'Password must be at least 8 characters'),
-  });
+      .required("Password is required")
+      .min(8, "Password must be at least 8 characters"),
+  })
 
   const formik = useFormik({
     initialValues: {
-      username: '',
-      email: '',
-      password: '',
+      username: "",
+      email: "",
+      password: "",
     },
     validationSchema: RegisterSchema,
     onSubmit: () => {
-      navigate('/dashboard', { replace: true });
+      navigate("/dashboard", { replace: true })
     },
-  });
+  })
 
-  const { errors, touched, handleSubmit, isSubmitting, getFieldProps } = formik;
+  const { errors, touched, handleSubmit, isSubmitting, getFieldProps } = formik
 
   return (
     <FormikProvider value={formik}>
       <Form autoComplete="off" noValidate onSubmit={handleSubmit}>
         <Stack spacing={3}>
           <TextField
+            data-cy="username"
             fullWidth
             label="Username"
-            {...getFieldProps('username')}
+            {...getFieldProps("username")}
             error={Boolean(touched.username && errors.username)}
             helperText={touched.username && errors.username}
           />
 
           <TextField
+            data-cy="email"
             fullWidth
-            autoComplete="username"
+            autoComplete="email"
             type="email"
             label="Email address"
-            {...getFieldProps('email')}
+            {...getFieldProps("email")}
             error={Boolean(touched.email && errors.email)}
             helperText={touched.email && errors.email}
           />
 
           <TextField
+            data-cy="password"
             fullWidth
-            autoComplete="current-password"
-            type={showPassword ? 'text' : 'password'}
+            type={showPassword ? "text" : "password"}
             label="Password"
-            {...getFieldProps('password')}
+            {...getFieldProps("password")}
             InputProps={{
               endAdornment: (
                 <InputAdornment position="end">
@@ -75,7 +82,7 @@ const Register = () => {
                     onClick={() => setShowPassword((prev) => !prev)}
                   >
                     <Iconify
-                      icon={showPassword ? 'eva:eye-fill' : 'eva:eye-off-fill'}
+                      icon={showPassword ? "eva:eye-fill" : "eva:eye-off-fill"}
                     />
                   </IconButton>
                 </InputAdornment>
@@ -86,6 +93,7 @@ const Register = () => {
           />
 
           <LoadingButton
+            data-cy="submitRegister"
             fullWidth
             size="large"
             type="submit"
@@ -97,7 +105,7 @@ const Register = () => {
         </Stack>
       </Form>
     </FormikProvider>
-  );
-};
+  )
+}
 
-export default Register;
+export default Register
