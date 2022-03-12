1ca945a867a6ef1be788bda0b353034beb6ac7ca
Isacc <iavitia@outlook.com>
2022-03-12 12:59:32 -0800

    mongoose setup

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ import express from 'express'
 // middleware
 import notFoundMiddleware from './middleware/not-found.js'
 import errorhandlerMiddleware from './middleware/error-handler.js'
+// db
+import connectDB from './db/connect.js'
 
 dotenv.config()
 const app = express()
@@ -16,6 +18,15 @@ app.use(errorhandlerMiddleware)
 
 const port = process.env.PORT || 5000
 
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`)
-})
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL)
+    app.listen(port, () => {
+      console.log(`Server listening on port ${port}`)
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+start()
