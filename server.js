ad365a99136982e81cd0a0a03990350acb0c37de
Isacc <iavitia@outlook.com>
2022-03-12 12:27:09 -0800

    404 & 500 middleare, dotenv setup

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
-import express from 'express';
+import dotenv from 'dotenv'
+import express from 'express'
+// middleware
+import notFoundMiddleware from './middleware/not-found.js'
+import errorhandlerMiddleware from './middleware/error-handler.js'
 
-const app = express();
+dotenv.config()
+const app = express()
 
 app.get('/', (req, res) => {
-  res.send('Welcome');
-});
+  res.send('Welcome')
+})
 
-const port = process.env.PORT || 5000;
+app.use(notFoundMiddleware)
+app.use(errorhandlerMiddleware)
+
+const port = process.env.PORT || 5000
 
 app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
-});
+  console.log(`Server listening on port ${port}`)
+})
