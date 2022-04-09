3d3d222d36f3ad03b8a895a45545bfd0a8987a89
Isacc <iavitia@outlook.com>
2022-04-08 22:04:08 -0700

    font source update

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,10 +10,10 @@ import { CollapseDrawerProvider } from './context/CollapseDrawerContext'
 // Scroll bar
 import 'simplebar/src/simplebar.css'
 
-import '@fontsource/roboto/300.css'
-import '@fontsource/roboto/400.css'
-import '@fontsource/roboto/500.css'
-import '@fontsource/roboto/700.css'
+// import '@fontsource/roboto/300.css'
+// import '@fontsource/roboto/400.css'
+// import '@fontsource/roboto/500.css'
+// import '@fontsource/roboto/700.css'
 
 ReactDOM.render(
   <React.StrictMode>
