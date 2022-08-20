1805c05a52153be735ad2b5b508116227cfea6c6
Isacc <iavitia@outlook.com>
2022-08-19 20:02:57 -0700

    mongoose user collation, case insensitive rules

--- models/User.js
+++ models/User.js
@@ -3,53 +3,56 @@ import validator from 'validator'
 import bcrypt from 'bcryptjs'
 import jwt from 'jsonwebtoken'
 
-const UserSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: [true, 'Username is required'],
-    match: [
-      /^[A-Za-z0-9_-]*$/,
-      'Letters, numbers, dashes, and underscores only. Please try again without symbols.'
-    ],
-    minlength: 3,
-    maxlength: 20,
-    trim: true,
-    unique: true
-  },
-  email: {
-    type: String,
-    required: [true, 'Email is required'],
-    validate: {
-      validator: validator.isEmail,
-      message: 'Enter a valid email address'
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: [true, 'Username is required'],
+      match: [
+        /^[A-Za-z0-9_-]*$/,
+        'Letters, numbers, dashes, and underscores only. Please try again without symbols.'
+      ],
+      minlength: 3,
+      maxlength: 20,
+      trim: true,
+      unique: true
     },
-    trim: true,
-    unique: true
+    email: {
+      type: String,
+      required: [true, 'Email is required'],
+      validate: {
+        validator: validator.isEmail,
+        message: 'Enter a valid email address'
+      },
+      trim: true,
+      unique: true
+    },
+    password: {
+      type: String,
+      required: [true, 'Password is required'],
+      minlength: 8,
+      maxlength: 64,
+      trim: true,
+      select: false
+    },
+    firstName: {
+      type: String,
+      trim: true,
+      default: ''
+    },
+    lastName: {
+      type: String,
+      trim: true,
+      default: ''
+    },
+    location: {
+      type: String,
+      trim: true,
+      default: ''
+    }
   },
-  password: {
-    type: String,
-    required: [true, 'Password is required'],
-    minlength: 8,
-    maxlength: 64,
-    trim: true,
-    select: false
-  },
-  firstName: {
-    type: String,
-    trim: true,
-    default: ''
-  },
-  lastName: {
-    type: String,
-    trim: true,
-    default: ''
-  },
-  location: {
-    type: String,
-    trim: true,
-    default: ''
-  }
-})
+  { collation: { locale: 'en', strength: 2 } }
+)
 
 UserSchema.pre('save', async function () {
   if (!this.isModified('password')) return
