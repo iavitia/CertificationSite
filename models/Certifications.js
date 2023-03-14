f6a649092ac15bf2dad9c1adf50323e96ebc3476
Isacc <iavitia@outlook.com>
2023-03-14 02:08:08 -0700

    schema date to timestamps

--- models/Certifications.js
+++ models/Certifications.js
@@ -1,92 +1,96 @@
 import mongoose from 'mongoose'
 
-const QuestionSchema = new mongoose.Schema({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User'
-  },
-  title: {
-    type: String,
-    required: [true, 'Title is required'],
-    minlength: 10,
-    maxlength: 150,
-    trim: true
-  },
-  question: {
-    type: String,
-    required: [true, 'Question is required'],
-    minlength: 50,
-    maxlength: 5000,
-    trim: true
-  },
-  choices: {
-    type: [String],
-    minlength: 10,
-    maxlength: 150,
-    required: [true, 'Choices are required'],
-    trim: true,
-    minitems: [2, 'At least 2 choices are required'],
-    maxitems: [6, 'At most 6 choices are allowed']
-  },
-  correctAnswer: {
-    type: [String],
-    required: [true, 'Correct answer is required'],
-    minItems: [1, 'At least 1 correct answer is required'],
-    maxItems: { $cond: { if: { $eq: ['$choices', 2] }, then: 1, else: null } },
-    validate: [
-      (correctAnswer, { choices }) => {
-        const correctAnswers = correctAnswer.map((answer) =>
-          answer.toLowerCase()
-        )
-        const choicesLowerCase = choices.map((choice) => choice.toLowerCase())
-        return correctAnswers.every((answer) =>
-          choicesLowerCase.includes(answer)
-        )
+const QuestionSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User'
+    },
+    title: {
+      type: String,
+      required: [true, 'Title is required'],
+      minlength: 10,
+      maxlength: 150,
+      trim: true
+    },
+    question: {
+      type: String,
+      required: [true, 'Question is required'],
+      minlength: 50,
+      maxlength: 5000,
+      trim: true
+    },
+    choices: {
+      type: [String],
+      minlength: 10,
+      maxlength: 150,
+      required: [true, 'Choices are required'],
+      trim: true,
+      minitems: [2, 'At least 2 choices are required'],
+      maxitems: [6, 'At most 6 choices are allowed']
+    },
+    correctAnswer: {
+      type: [String],
+      required: [true, 'Correct answer is required'],
+      minItems: [1, 'At least 1 correct answer is required'],
+      maxItems: {
+        $cond: { if: { $eq: ['$choices', 2] }, then: 1, else: null }
       },
-      'Correct answer must be in choices'
+      validate: [
+        (correctAnswer, { choices }) => {
+          const correctAnswers = correctAnswer.map((answer) =>
+            answer.toLowerCase()
+          )
+          const choicesLowerCase = choices.map((choice) => choice.toLowerCase())
+          return correctAnswers.every((answer) =>
+            choicesLowerCase.includes(answer)
+          )
+        },
+        'Correct answer must be in choices'
+      ]
+    },
+    solution: {
+      type: String,
+      required: [true, 'Solution is required'],
+      minlength: 50,
+      maxlength: 10000
+    },
+    difficulty: {
+      type: String,
+      required: [true, 'Difficulty is required'],
+      enum: ['easy', 'medium', 'hard'],
+      trim: true
+    },
+    upvotes: [
+      {
+        user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
+      }
+    ],
+    downvotes: [
+      {
+        user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
+      }
+    ],
+    comments: [
+      {
+        user: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'User'
+        },
+        comment: {
+          type: String,
+          required: [true, 'Comment is required'],
+          minlength: 10,
+          maxlength: 500,
+          trim: true
+        },
+
+        date: { type: Date, default: Date.now }
+      }
     ]
   },
-  solution: {
-    type: String,
-    required: [true, 'Solution is required'],
-    minlength: 50,
-    maxlength: 10000
-  },
-  difficulty: {
-    type: String,
-    required: [true, 'Difficulty is required'],
-    enum: ['easy', 'medium', 'hard'],
-    trim: true
-  },
-  upvotes: [
-    {
-      user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
-    }
-  ],
-  downvotes: [
-    {
-      user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
-    }
-  ],
-  comments: [
-    {
-      user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
-      },
-      comment: {
-        type: String,
-        required: [true, 'Comment is required'],
-        minlength: 10,
-        maxlength: 500,
-        trim: true
-      },
-
-      date: { type: Date, default: Date.now }
-    }
-  ],
-  date: { type: Date, default: Date.now }
-})
+  { timestamps: true }
+)
 
 const SectionSchema = new mongoose.Schema({
   section: {
